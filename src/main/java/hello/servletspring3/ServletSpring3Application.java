e77622ab67ae445eac2cb21fedb6beb41212b675
Sully <2dcoder@naver.com>
2024-03-30 23:23:41 +0900

    initial commit

---
+++ src/main/java/hello/servletspring3/ServletSpring3Application.java
@@ -0,0 +1,13 @@
+package hello.servletspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletSpring3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletSpring3Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/servletspring3/ServletSpring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.servletspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletSpring3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
