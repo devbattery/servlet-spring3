c769e9e2a65010c78f76f646d08d4a0a6b77c581
Sully <2dcoder@naver.com>
2024-03-31 20:10:31 +0900

    섹션 2: 서블릿
    HttpServletRequest - 개요

--- src/main/java/hello/servletspring3/ServletSpring3Application.java
+++ src/main/java/hello/servletspring3/ServletSpring3Application.java
@@ -2,7 +2,9 @@ package hello.servletspring3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동 등록 애노테이션
 @SpringBootApplication
 public class ServletSpring3Application {
 
