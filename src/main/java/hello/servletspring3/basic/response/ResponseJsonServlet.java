876b76b9c2e57371625a0a28821694266c25a054
Sully <2dcoder@naver.com>
2024-04-10 22:10:39 +0900

    섹션 2: 서블릿
    HTTP 응답 데이터 - API JSON

---
+++ src/main/java/hello/servletspring3/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servletspring3.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servletspring3.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Content-Type: application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("sully");
+        helloData.setAge(22);
+
+        // {"username": "sully", "age": 22}
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
