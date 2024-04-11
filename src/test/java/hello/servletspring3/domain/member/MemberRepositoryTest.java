892c30cff5cd1ce26dc7ae8b96a1ae733b33b234
Sully <2dcoder@naver.com>
2024-04-11 20:32:41 +0900

    섹션 3: 서블릿, JSP, MVC 패턴
    회원 관리 웹 애플리케이션 요구사항

---
+++ src/test/java/hello/servletspring3/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package hello.servletspring3.domain.member;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryTest {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @DisplayName("")
+    @Test
+    void save() {
+        // given
+        Member member = new Member("hello", 20);
+
+        // when
+        Member savedMember = memberRepository.save(member);
+
+        // then
+        Member foundMember = memberRepository.findById(savedMember.getId());
+        assertThat(foundMember).isEqualTo(savedMember);
+    }
+
+    @DisplayName("")
+    @Test
+    void find_all() {
+        // given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 22);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        // when
+        List<Member> members = memberRepository.findAll();
+
+        // then
+        assertThat(members).hasSize(2);
+        assertThat(members).contains(member1, member2);
+    }
+
+}
