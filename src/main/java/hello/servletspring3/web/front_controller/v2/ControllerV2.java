0734ef68a993fa05804b9e9b0457a4d70798aeca
Sully <2dcoder@naver.com>
2024-04-17 22:38:21 +0900

    섹션 4: MVC 프레임워크 만들기
    View 분리 - v2

---
+++ src/main/java/hello/servletspring3/web/front_controller/v2/ControllerV2.java
@@ -0,0 +1,17 @@
+package hello.servletspring3.web.front_controller.v2;
+
+import hello.servletspring3.web.front_controller.MyView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    /**
+     * 서블릿 메서드랑 똑같은 모양의 메서드
+     * @return MyView
+     */
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+}
---
+++ src/main/java/hello/servletspring3/web/front_controller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,26 @@
+package hello.servletspring3.web.front_controller.v2.controller;
+
+import hello.servletspring3.domain.member.Member;
+import hello.servletspring3.domain.member.MemberRepository;
+import hello.servletspring3.web.front_controller.MyView;
+import hello.servletspring3.web.front_controller.v2.ControllerV2;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+
+}
